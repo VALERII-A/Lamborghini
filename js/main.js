f53bcdfa61cf9950cacbf64895d19acd628f12cf
VALERII-A <saligasaliga100@gmail.com>
2024-02-20 14:05:46 +0500

    map

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+((g) => {
+  var h,
+    a,
+    k,
+    p = "The Google Maps JavaScript API",
+    c = "google",
+    l = "importLibrary",
+    q = "__ib__",
+    m = document,
+    b = window;
+  b = b[c] || (b[c] = {});
+  var d = b.maps || (b.maps = {}),
+    r = new Set(),
+    e = new URLSearchParams(),
+    u = () =>
+      h ||
+      (h = new Promise(async (f, n) => {
+        await (a = m.createElement("script"));
+        e.set("libraries", [...r] + "");
+        for (k in g)
+          e.set(
+            k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
+            g[k]
+          );
+        e.set("callback", c + ".maps." + q);
+        a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+        d[q] = f;
+        a.onerror = () => (h = n(Error(p + " could not load.")));
+        a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+        m.head.append(a);
+      }));
+  d[l]
+    ? console.warn(p + " only loads once. Ignoring:", g)
+    : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+})({
+  key: "AIzaSyDHVyB-dRW3fhSdUmy3lqoiaNjV9JVUR4M",
+});
 const swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 2,
@@ -11,8 +48,10 @@ const swiper = new Swiper(".swiper", {
 
 let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
     center: { lat: 37.23283907666491, lng: -8.628786428281618 },
     zoom: 14,
     styles: [
@@ -143,4 +182,4 @@ function initMap() {
   });
 }
 
-window.initMap = initMap;
+initMap();
